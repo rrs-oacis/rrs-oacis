52f288d8d3f98e2b2afe5a7ce5e6d510f4b7a4b2
k14041kk <k14041kk@aitech.ac.jp>
2017-04-26 16:45:48 +0900

    変更：名前

--- src/view/component/main-footer.php
+++ src/view/component/main-footer.php
@@ -6,6 +6,6 @@ use adf\Config;
 <div class="pull-right hidden-xs">
 <b>Version</b> <?= Config::APP_VERSION ?>
 </div>
-<strong>Copyright &copy; 2017 K14041kk kamiya. </strong> All rights
+<strong>Copyright &copy; 2017 ADF Team. </strong> All rights
 reserved.
 </footer>
