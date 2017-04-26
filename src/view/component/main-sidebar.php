e25708c65711f03ae9515617af39dbafa803f8dd
k14041kk <k14041kk@aitech.ac.jp>
2017-04-26 16:43:13 +0900

    追加：必要ない処理を削除

--- src/view/component/main-sidebar.php
+++ src/view/component/main-sidebar.php
@@ -6,6 +6,7 @@ use adf\Config;
   <section class="sidebar">
     
     <!-- search form -->
+    <!-- 
     <form action="./search.php" method="get" class="sidebar-form">
       <div class="input-group">
         <input type="text" name="q" class="form-control" placeholder="検索..."> <span class="input-group-btn">
@@ -15,6 +16,7 @@ use adf\Config;
         </span>
       </div>
     </form>
+     -->
     <!-- /.search form -->
     <!-- sidebar menu: : style can be found in sidebar.less -->
     <ul class="sidebar-menu">
@@ -22,6 +24,7 @@ use adf\Config;
       <li><a href="<?=Config::$TOP_PATH ?>index.php"><i class="fa fa-dashboard"></i> <span>ダッシュボード</span></a></li>
       <li><a href="<?=Config::$TOP_PATH ?>agents"><i class="fa fa-book"></i> <span>エージェント一覧</span></a></li>
       
+      <!-- 
       <li><a href="./week.php"> <i class="fa fa-table"></i> <span>予定</span> <span class="pull-right-container"> 
             <small id="sidebar-week-label-green" class="label pull-right bg-green">-</small>
             <small id="sidebar-week-label-red" class="label pull-right bg-red">-</small>
@@ -35,12 +38,14 @@ use adf\Config;
             <small id="sidebar-month-label-blue" class="label pull-right bg-blue">-</small>
         </span>
       </a></li>
+       -->
            
-      
+      <!-- 
       <li class="header">ラベル</li>
       <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>達成</span></a></li>
       <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>期限</span></a></li>
       <li><a href="#"><i class="fa fa-circle-o text-green"></i> <span>達成</span></a></li>
+       -->
       
     </ul>
   </section>
