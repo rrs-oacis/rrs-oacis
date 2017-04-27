0ad796b61cd429c9a9a41d47a429c0fbef285a1c
k14041kk <k14041kk@aitech.ac.jp>
2017-04-27 13:42:45 +0900

    追加：日本語lang

--- src/locale/ja.php
+++ src/locale/ja.php
@@ -4,9 +4,33 @@ namespace adf\locale;
 class ja{
 	
 	public static $l = array(
-			"adf.dashboard2"  => "ダッシュボード",
-			"adf.agent_list2"  => "エージェント一覧",
-			"adf.setting2"  => "設定"
+			//Main
+			"adf.dashboard"  => "ダッシュボード",
+			"adf.agent_list"  => "エージェント一覧",
+			"adf.agent"  => "エージェント",
+			"adf.setting"  => "設定",
+			"adf.operation_screen" => "操作画面",
+			"adf.index"  => "Index",
+			//Box add-agent
+			"adf.add_agent_box.add_agent" => "エージェントを追加",
+			"adf.add_agent_box.agent_name" => "名前",
+			"adf.add_agent_box.agent_file" => "ファイル",
+			"adf.add_agent_box.input_name" => "name",
+			"adf.add_agent_box.input_not_file" => "選択されていません",
+			"adf.add_agent_box.input_browse" => "選択",
+			"adf.add_agent_box.input_add" => "追加",
+			"adf.add_agent_box.input_cancel" => "Cancel",
+			//Box agent-list
+			"adf.agents_list_box.uuid" => "UUID",
+			"adf.agents_list_box.name" => "エージェント名",
+			"adf.agents_list_box.upload_day" => "アップロード日",
+			"adf.agents_list_box.status" => "ステータス",
+			"adf.agents_list_box.link" => "リンク",
+			"adf.agents_list_box.details" => "詳細情報",
+			//
+			"adf.agent_box.agent" => "エージェント",
+			"adf.agent_box.name" => "名前"
+			
 	);
 	
 }
