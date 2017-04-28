4565fd415319f11f29f504b4fc7dcec3f3fb7bee
k14041kk <k14041kk@aitech.ac.jp>
2017-04-28 11:43:02 +0900

    追加：sudoを削除

--- docker_run.bat
+++ docker_run.bat
@@ -2,6 +2,6 @@ cd /d %~dp0
 
 set BATDIR=%~dp0
 
-sudo docker run --rm -p 6040:6040 -v %BATDIR%src:/adf/src -v %BATDIR%public:/adf/public -v %BATDIR%ruby:/adf/ruby -t -i test/test:1.0  /bin/bash
+docker run --rm -p 6040:6040 -v %BATDIR%src:/adf/src -v %BATDIR%public:/adf/public -v %BATDIR%ruby:/adf/ruby -t -i test/test:1.0  /bin/bash
 
 pause
