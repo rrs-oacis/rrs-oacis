d4986b3a10064171d6835b705ad5b56ba9ec1b9f
k14041kk <k14041kk@aitech.ac.jp>
2017-04-29 17:32:25 +0900

    追加：エージェントもマウントされるように

--- docker_run.sh
+++ docker_run.sh
@@ -2,4 +2,4 @@
 
 cd `dirname $0`
 
-sudo docker run --rm -p 6040:6040 -p 127.0.0.1:3000:3000 -v $(pwd)/src:/home/oacis/adf/src -v $(pwd)/public:/home/oacis/adf/public -v $(pwd)/ruby:/home/oacis/adf/ruby -t -i test/test:1.0
+sudo docker run --rm -p 6040:6040 -p 127.0.0.1:3000:3000 -v $(pwd)/src:/home/oacis/adf/src -v $(pwd)/public:/home/oacis/adf/public -v $(pwd)/ruby:/home/oacis/adf/ruby -v $(pwd)/agents:/home/oacis/adf/rrsenv/AGENT -t -i test/test:1.0
