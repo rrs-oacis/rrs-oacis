95808a8a628631b23e39749e0e87dfab807a6ed2
Shunki Takami <tkmnet@users.noreply.github.com>
2017-05-01 00:16:53 +0900

    Fix: docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -2,5 +2,5 @@
 
 cd `dirname $0`
 
-sudo docker run --rm -p 6040:6040 -p 127.0.0.1:3000:3000 -v $(pwd)/src:/home/oacis/adf/src -v $(pwd)/public:/home/oacis/adf/public -v $(pwd)/ruby:/home/oacis/adf/ruby -v $(pwd)/agents:/home/oacis/adf/rrsenv/AGENT -t -i rrsoacis/rrsoacis:latest
+docker run --rm -p 6040:6040 -p 127.0.0.1:3000:3000 -v $(pwd)/src:/home/oacis/adf/src -v $(pwd)/public:/home/oacis/adf/public -v $(pwd)/ruby:/home/oacis/adf/ruby -v $(pwd)/agents:/home/oacis/adf/rrsenv/AGENT -t -i rrsoacis/rrsoacis:latest
 
