5409a7c22a795609c21d2c8ffea37bcd9c75a2c8
Shunki Takami <tkmnet@users.noreply.github.com>
2017-05-01 02:32:44 +0900

    Change: base directory

--- docker_run.sh
+++ docker_run.sh
@@ -2,5 +2,5 @@
 
 cd `dirname $0`
 
-docker run --rm -p 3080:3080 -p 127.0.0.1:3000:3000 -v $(pwd)/src:/home/oacis/adf/src -v $(pwd)/public:/home/oacis/adf/public -v $(pwd)/ruby:/home/oacis/adf/ruby -v $(pwd)/agents:/home/oacis/adf/rrsenv/AGENT -t -i rrsoacis/rrsoacis:latest
+docker run --rm -p 3080:3080 -p 127.0.0.1:3000:3000 -v $(pwd)/src:/home/oacis/rrs-oacis/src -v $(pwd)/public:/home/oacis/rrs-oacis/public -v $(pwd)/ruby:/home/oacis/rrs-oacis/ruby -v $(pwd)/agents:/home/oacis/rrs-oacis/rrsenv/AGENT -t -i rrsoacis/rrsoacis:latest
 
