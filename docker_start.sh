d27c3704b0e0f214ac1599ea2e40d65cd9fb3b5b
Shunki Takami <tkmnet@users.noreply.github.com>
2017-05-01 01:23:34 +0900

    Fix: docker_start.sh

--- docker_start.sh
+++ docker_start.sh
@@ -2,6 +2,6 @@
 
 cd `dirname $0`
 
-docker rm -f rrsoacis 2>&1 >/dev/null
+docker rm -f rrsoacis >/dev/null 2>&1
 docker create --rm -p 3080:3080 -p 127.0.0.1:3000:3000 -v $(pwd)/src:/home/oacis/adf/src -v $(pwd)/public:/home/oacis/adf/public -v $(pwd)/ruby:/home/oacis/adf/ruby -v $(pwd)/agents:/home/oacis/adf/rrsenv/AGENT --name rrsoacis -i rrsoacis/rrsoacis:latest
 docker start rrsoacis
