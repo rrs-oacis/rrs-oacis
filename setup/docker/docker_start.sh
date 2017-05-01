ec05314dea619f52886f45afeace36a96c1f8d86
Shunki Takami <tkmnet@users.noreply.github.com>
2017-05-02 02:06:38 +0900

    Update: docker_start.sh

--- setup/docker/docker_start.sh
+++ setup/docker/docker_start.sh
@@ -3,5 +3,5 @@
 cd `dirname $0`
 
 docker rm -f rrsoacis >/dev/null 2>&1
-docker create --rm -p 3080:3080 -p 127.0.0.1:3000:3000 -v $(pwd)/src:/home/oacis/rrs-oacis/src -v $(pwd)/public:/home/oacis/rrs-oacis/public -v $(pwd)/ruby:/home/oacis/rrs-oacis/ruby -v $(pwd)/agents:/home/oacis/rrs-oacis/rrsenv/AGENT --name rrsoacis -i rrsoacis/rrsoacis:latest
+docker create --rm --name rrsoacis -i rrsoacis/rrsoacis:latest
 docker start rrsoacis
