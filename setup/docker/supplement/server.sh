50a3cd49810e4b67def8e8511a2bbf98dd05a220
Shunki Takami <tkmnet@users.noreply.github.com>
2017-05-02 02:00:19 +0900

    Add: init function

--- setup/docker/supplement/server.sh
+++ setup/docker/supplement/server.sh
@@ -2,5 +2,11 @@
 
 cd `dirname $0`
 
+if [ rrsoacis-init.sh -a ! -e .rrs-oacis.initialized ] ; then
+    chmod a+x rrsoacis-init.sh
+    ./rrsoacis-init.sh
+fi
+touch .rrs-oacis.initialized
+
 /home/oacis/oacis_start.sh &
 php -S 0.0.0.0:3080 -t public -c php.ini public/_app.php
