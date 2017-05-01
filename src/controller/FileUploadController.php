18afc508b1afb3bb311131b8f608090f2715b712
k14041kk <k14041kk@aitech.ac.jp>
2017-05-01 10:08:18 +0900

    追加：エージェントの名前を付けるように

--- src/controller/FileUploadController.php
+++ src/controller/FileUploadController.php
@@ -22,7 +22,8 @@ class FileUploadController extends AbstractController {
 		self::extractZip($uuid);
 		
 		//TODO オアシスに登録処理
-		$output = shell_exec("sh ". Config::$ROUTER_PATH. "ruby/add_agent.sh test " . $uuid);
+		$fileName = $_POST['agent_name'];
+		$output = shell_exec("sh ". Config::$ROUTER_PATH. "ruby/add_agent.sh test " .$fileName."_".$uuid);
 		
 		
 	}
