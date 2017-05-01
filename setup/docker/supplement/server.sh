c95475896d59a521bafdb382cce7196c86766ac8
Shunki Takami <tkmnet@users.noreply.github.com>
2017-05-02 02:14:03 +0900

    Update: supplement/server.sh

--- setup/docker/supplement/server.sh
+++ setup/docker/supplement/server.sh
@@ -2,11 +2,15 @@
 
 cd `dirname $0`
 
-if [ rrsoacis-init.sh -a ! -e .rrs-oacis.initialized ] ; then
+BASEDIR=`pwd`
+cd /home/oacis
+if [ ./rrsoacis-init.sh -a ! -e .rrs-oacis.initialized ] ; then
     chmod a+x rrsoacis-init.sh
     ./rrsoacis-init.sh
 fi
 touch .rrs-oacis.initialized
+cd $BASEDIR
 
 /home/oacis/oacis_start.sh &
 php -S 0.0.0.0:3080 -t public -c php.ini public/_app.php
+
