09b4268cab585c46f74ac3774aba947fee3c34b9
k14041kk <k14041kk@aitech.ac.jp>
2017-05-01 15:03:17 +0900

    追加：ボックスをたためるように

--- src/view/component/box-add_parameter.php
+++ src/view/component/box-add_parameter.php
@@ -4,6 +4,10 @@ use adf\Config;
 <div class="box box-info">
   <div class="box-header with-border">
     <h3 class="box-title"><?= _l("adf.add_agent_box.add_parameter"); ?></h3>
+    <div class="box-tools pull-right">
+        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+        </button>
+     </div>
   </div>
   <!-- /.box-header -->
   <!-- form start -->
