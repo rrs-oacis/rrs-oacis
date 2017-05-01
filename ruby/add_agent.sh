d2ff91f971375eb6a75781f778ba55eb36d274b7
k14041kk <k14041kk@aitech.ac.jp>
2017-05-01 16:15:35 +0900

    追加：shを改良

--- ruby/add_agent.sh
+++ ruby/add_agent.sh
@@ -13,4 +13,4 @@ A=$3 #$4
 RCMD=`echo '{"num_runs":5,"mpi_procs":0,"omp_threads":0,"priority":1,"submitted_to":"${HOST}","host_parameters":null}'`
 #/home/oacis/oacis/bin/oacis_cli create_parameter_sets -s ${SIML} -r ${RCMD} -o "/tmp/ps-`date`-${RANDOM}.json" -i "{\"MAP\":\"${MAP}\",\"F\":\"${F}\",\"P\":\"${P}\",\"A\":\"${A}\"}"
 
-/home/oacis/oacis/bin/oacis_cli create_parameter_sets -s 590463aee4dec200d962035a -i '{"MAP":"'${MAP}'","F":"'${F}'","P":"'${P}'","A":"'${A}'"}' -r '{"num_runs":1,"mpi_procs":0,"omp_threads":0,"priority":1,"submitted_to":${HOST},"host_parameters":null}' -o ps.json
+/home/oacis/oacis/bin/oacis_cli create_parameter_sets -s ${SIML} -i '{"MAP":"'${MAP}'","F":"'${F}'","P":"'${P}'","A":"'${A}'"}' -r '{"num_runs":1,"mpi_procs":0,"omp_threads":0,"priority":1,"submitted_to":${HOST},"host_parameters":null}' -o ps.json
