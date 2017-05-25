e469efaee89be79af8059eebf02d784ee2f033d8
Shunki Takami <tkmnet@users.noreply.github.com>
2017-05-25 06:24:23 +0000

    Update: server.sh

--- server.sh
+++ server.sh
@@ -2,4 +2,12 @@
 
 cd `dirname $0`
 
+if [ /etc/rrsoacis-init.sh -a ! -e /home/oacis/rrs-oacis/data/.rrs-oacis.initialized ] ; then
+    chmod a+x /etc/rrsoacis-init.sh
+    /etc/rrsoacis-init.sh
+fi
+touch /home/oacis/rrs-oacis/data/.rrs-oacis.initialized
+
+/home/oacis/oacis_start.sh &
 php -S 0.0.0.0:3080 -t public -c php.ini public/_app.php
+
