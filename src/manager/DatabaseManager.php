b939d421534350048e6317229dbbf5c552273c9b
Shunki Takami <tkmnet@users.noreply.github.com>
2017-10-06 15:08:00 +0900

    Update: DatabaseManager

--- src/manager/DatabaseManager.php
+++ src/manager/DatabaseManager.php
@@ -32,7 +32,7 @@ class DatabaseManager
             mkdir(Config::$ROUTER_PATH."data",0777,true);
         }
 
-        $db = new PDO('sqlite:'.Config::$ROUTER_PATH."data/main.db");
+        $db = new PDO('sqlite:'.Config::$ROUTER_PATH."data/_main.db");
         $sth = $db->query("select * from sqlite_master where name='system';");
         if (0 >= $sth->rowCount())
         {
