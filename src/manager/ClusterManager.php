60f5956a7872af7be16c414f643354520a669ffc
Shunki Takami <tkmnet@users.noreply.github.com>
2017-10-06 16:06:19 +0900

    Clean: ClusterManager

--- src/manager/ClusterManager.php
+++ src/manager/ClusterManager.php
@@ -30,40 +30,6 @@ class ClusterManager
 		return $clusters;
 	}
 
-	public static function getAgent($uuid) {
-		
-		$agentDir = Config::$ROUTER_PATH . Config::AGENTS_DIR_NAME;
-		$agentFile = $agentDir . "/" . $uuid;
-		
-		$files = scandir ( $agentDir );
-		$files = array_filter ( $files, function ($file) { // 注(1)
-			return ! in_array ( $file, array (
-					'.',
-					'..' 
-			) );
-		} );
-		
-		foreach ( $files as $file ) {
-			
-			//File名のUUIDが一致するか　
-			if(preg_match("/".$uuid."$/",$file)){
-				
-				$json = file_get_contents ( $agentDir . "/" . $file . "/" . Config::AGENT_META_JSON );
-				$json = mb_convert_encoding ( $json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN' );
-				$obj = json_decode ( $json, true );
-				$obj ['upload_date'] = date ( "Y年m月d日 H時i分s秒", $obj ['upload_date'] );
-				
-				return $obj;
-				
-			}
-			
-		}
-		
-		throw new AgentNotFoundException('Not Found Agent : UUID = '.$uuid);
-		
-		return null;
-	}
-
     public static function getMainHostGroup()
     {
         $db = self::connectDB();
