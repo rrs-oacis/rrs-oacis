0d8349146cf09250082819658944988c8e513841
k14041kk <k14041kk@aitech.ac.jp>
2017-10-31 15:07:10 +0900

    Add: Map Multi Upload

---
+++ apps/rrs_oacis/map_multi_upload/MapUploadMainController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: k14041kk
+ * Date: 2017/10/06
+ * Time: 14:04
+ */
+namespace rrsoacis\apps\rrs_oacis\map_multi_upload;
+
+use rrsoacis\system\Config;
+use rrsoacis\component\common\AbstractController;
+use rrsoacis\manager\AppManager;
+use rrsoacis\manager\MapManager;
+use rrsoacis\manager\AgentManager;
+
+class MapUploadMainController extends AbstractController
+{
+    public function get()
+    {
+
+        include(dirname(__FILE__) . '/MapUploadMainView.php');
+    }
+}
+?>
