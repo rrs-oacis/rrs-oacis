65b2d2e0f1a4cc7141e5e23512d19cea1e65e1bd
Shunki TAKAMI(takamin@sylph.maslab) <tkmnet@users.noreply.github.com>
2017-12-04 15:19:10 +0900

    Fix: prepare_sandbox.sh

--- setup/docker/prepare_sandbox.sh
+++ setup/docker/prepare_sandbox.sh
@@ -5,7 +5,7 @@ cd ../../
 
 cp ./setup/docker/supplement/server.sh ./
 
-docker create --name rrsoacis_sandbox -p 3080:3080 -p 3000:3000 -p 49138:49138/udp -v `pwd`:/home/oacis/rrs-oacis -dt rrsoacis/rrsoacis
+docker create --name rrsoacis_sandbox -p 3080:3080 -p 3000:3000 -p 49138:49138/udp -v `pwd`:/home/oacis/rrs-oacis -t rrsoacis/rrsoacis
 
 echo RRS-OACIS sandbox is created as rrsoacis_sandbox.
 echo You can start the sandbox using following command.
