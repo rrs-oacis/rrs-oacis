782a1f4a7ac22c92e3e72b6ab0863f1f39f821af
Shunki TAKAMI(takamin@sylph.maslab) <tkmnet@users.noreply.github.com>
2017-12-04 13:00:14 +0900

    Update: prepare_sandbox.sh

--- setup/docker/prepare_sandbox.sh
+++ setup/docker/prepare_sandbox.sh
@@ -3,6 +3,8 @@
 cd `dirname $0`
 cd ../../
 
+cp ./setup/docker/supplement/server.sh ./
+
 docker create --name rrsoacis_sandbox -p 3080:3080 -p 3000:3000 -p 49138:49138/udp -v `pwd`:/home/oacis/rrs-oacis -dt rrsoacis/rrsoacis
 
 echo RRS-OACIS sandbox is created as rrsoacis_sandbox.
