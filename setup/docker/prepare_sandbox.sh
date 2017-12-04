30eb3090960e150957801c4e02c513a82109dc76
Shunki TAKAMI(takamin@sylph.maslab) <tkmnet@users.noreply.github.com>
2017-12-04 15:40:12 +0900

    Fix: typo on prepare_sandbox.sh

--- setup/docker/prepare_sandbox.sh
+++ setup/docker/prepare_sandbox.sh
@@ -8,7 +8,7 @@ chmod a+x ./server.sh
 cp ./setup/setup.sh ./rrsoacis-init.sh
 
 docker rm -f rrsoacis_sandbox
-docker create --name rrsoacis_sandbox -p 3080:3080 -p 3000:3000 -p 49138:49138/udp -v `pwd`:/home/oacis/rrs-oacisi -v `pwd`/rrsoacis-init.sh:/etc/rrsoacis-init.sh -t rrsoacis/rrsoacis
+docker create --name rrsoacis_sandbox -p 3080:3080 -p 3000:3000 -p 49138:49138/udp -v `pwd`:/home/oacis/rrs-oacis -v `pwd`/rrsoacis-init.sh:/etc/rrsoacis-init.sh -t rrsoacis/rrsoacis
 
 echo RRS-OACIS sandbox is created as rrsoacis_sandbox.
 echo You can start the sandbox using following command.
