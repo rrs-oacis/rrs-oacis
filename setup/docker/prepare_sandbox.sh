7791f52afca173462bb95cd9627ab3d4d872c52e
Shunki TAKAMI(takamin@sylph.maslab) <tkmnet@users.noreply.github.com>
2017-12-04 15:22:11 +0900

    Fix: prepare_sandbox.sh

--- setup/docker/prepare_sandbox.sh
+++ setup/docker/prepare_sandbox.sh
@@ -4,6 +4,7 @@ cd `dirname $0`
 cd ../../
 
 cp ./setup/docker/supplement/server.sh ./
+chmod a+x ./server.sh
 
 docker rm -f rrsoacis_sandbox
 docker create --name rrsoacis_sandbox -p 3080:3080 -p 3000:3000 -p 49138:49138/udp -v `pwd`:/home/oacis/rrs-oacis -t rrsoacis/rrsoacis
