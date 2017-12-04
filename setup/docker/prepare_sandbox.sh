692aba3dda7f2aa1cf3dbbd07c9ad06f127b6857
Shunki TAKAMI(takamin@sylph.maslab) <tkmnet@users.noreply.github.com>
2017-12-04 15:35:08 +0900

    Update: .gitignore and prepare_sandbox.sh

--- setup/docker/prepare_sandbox.sh
+++ setup/docker/prepare_sandbox.sh
@@ -5,9 +5,10 @@ cd ../../
 
 cp ./setup/docker/supplement/server.sh ./
 chmod a+x ./server.sh
+cp ./setup/setup.sh ./rrsoacis-init.sh
 
 docker rm -f rrsoacis_sandbox
-docker create --name rrsoacis_sandbox -p 3080:3080 -p 3000:3000 -p 49138:49138/udp -v `pwd`:/home/oacis/rrs-oacis -t rrsoacis/rrsoacis
+docker create --name rrsoacis_sandbox -p 3080:3080 -p 3000:3000 -p 49138:49138/udp -v `pwd`:/home/oacis/rrs-oacisi -v `pwd`/rrsoacis-init.sh:/etc/rrsoacis-init.sh -t rrsoacis/rrsoacis
 
 echo RRS-OACIS sandbox is created as rrsoacis_sandbox.
 echo You can start the sandbox using following command.
