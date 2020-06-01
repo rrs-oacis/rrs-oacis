6aca2fa0374102f74cf51d7e04216902f56bb242
uiiaoo <k15121kk@maslab.aitech.ac.jp>
2020-06-01 17:30:40 +0900

    RESOLVE some bugs on oacis-queue/main.pl

--- setup/docker/supplement/server.sh
+++ setup/docker/supplement/server.sh
@@ -3,24 +3,24 @@
 cd `dirname $0`
 
 function fixMongoDB() {
-	#waiting for mongod boot
-	until [ "$(mongo --eval 'printjson(db.serverStatus().ok)' | tail -1 | tr -d '\r')" == "1" ]
-	do
-		sleep 1
-		echo "waiting for mongod boot..."
-	done
-	mongo --eval 'db.adminCommand( { setParameter: 1, failIndexKeyTooLong: false } )'
+    #waiting for mongod boot
+    until [ "$(mongo --eval 'printjson(db.serverStatus().ok)' | tail -1 | tr -d '\r')" == "1" ]
+    do
+        sleep 1
+        echo "waiting for mongod boot..."
+    done
+    mongo --eval 'db.adminCommand( { setParameter: 1, failIndexKeyTooLong: false } )'
 }
 fixMongoDB &
 
-if [ -e /etc/rrsoacis-init.sh -a ! -e /home/oacis/rrs-oacis/data/.rrs-oacis.initialized ] ; then
+if [ -e /etc/rrsoacis-init.sh -a ! -e /home/oacis/rrs-oacis/data/.rrs-oacis.initialized ]; then
     chmod a+x /etc/rrsoacis-init.sh
     su oacis -c /etc/rrsoacis-init.sh
 fi
 touch /home/oacis/rrs-oacis/data/.rrs-oacis.initialized
+rm --force --recursive /home/oacis/rrs-oacis/oacis-queue/queue.lock
 
 /etc/init.d/php7.0-fpm restart
 /etc/init.d/nginx restart
 
 /home/oacis/oacis_start.sh
-
