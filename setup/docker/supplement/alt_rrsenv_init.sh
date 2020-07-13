9b3d0d68a7bfd8c461a278e934e7162c40bfeb11
uiiaoo <k15121kk@maslab.aitech.ac.jp>
2020-07-14 01:47:00 +0900

    FIX for the bug the server cannot shut down on a map image generation

--- setup/docker/supplement/alt_rrsenv_init.sh
+++ setup/docker/supplement/alt_rrsenv_init.sh
@@ -5,3 +5,4 @@ mkdir workspace AGENT MAP LOG
 git clone https://github.com/roborescue/rcrs-server.git roborescue
 sed --in-place 's/apache-ant-\*\/bin\/ant .*"/gradlew completeBuild"/' \
     script/rrscluster
+sed --in-place '/connect-time/s/^/#/' script/rrscluster
